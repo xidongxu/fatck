ac02d1ba263be5590fd9d7fd74dfc44940ac209a
xidongxu <xi_dongxu@126.com>
2023-06-10 16:34:55 +0800

    【修改】 设备操作接口；
    Signed-off-by: xidongxu <xi_dongxu@126.com>

--- fatdev.c
+++ fatdev.c
@@ -1,2 +1,27 @@
 ﻿// fatdev.c : fat device operate source file
 #include "fatdev.h"
+
+void* fat_dev_open(const char* name)
+{
+	return 0;
+}
+
+int fat_dev_read(void *dev, uint8_t *buff, size_t size)
+{
+	return 0;
+}
+
+int fat_dev_write(void* dev, uint8_t *buff, size_t size)
+{
+	return 0;
+}
+
+int fat_dev_close(void* dev)
+{
+	return 0;
+}
+
+int fat_dev_ctrl(void* dev, int cmd, void *args)
+{
+	return 0;
+}
--- fatdev.h
+++ fatdev.h
@@ -2,20 +2,12 @@
 #ifndef __FATDEV_H__
 #define __FATDEV_H__
 
+#include <stdint.h>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void* fat_dev_open(const char* name);
+int fat_dev_read(void* dev, uint8_t* buff, size_t size);
+int fat_dev_write(void* dev, uint8_t* buff, size_t size);
+int fat_dev_close(void* dev);
+int fat_dev_ctrl(void* dev, int cmd, void* args);
 
 #endif /* __FATDEV_H__ */
